a2a32ad60472e373a1287ec963abc7f47cdd9991
zackdreitzler <zdreitzler@gmail.com>
2018-09-10 10:11:06 -0400

    Moving Intellij stuff over to other workstation

---
+++ src/com/zack/dataModel/Main.java
@@ -0,0 +1,8 @@
+package com.zack.dataModel;
+
+public class Main {
+    public static void main(String[] args) {
+        ShiftData shiftData = new ShiftData();
+        shiftData.readContacts();
+    }
+}
