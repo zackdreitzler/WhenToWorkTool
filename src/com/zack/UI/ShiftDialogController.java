acf71806c6a61eb7394c94893f9518cdafe9b9ee
ifsohn <zdreitzler@gmail.com>
2018-09-14 16:39:35 -0400

    added dialog functionallity for buttons

--- src/com/zack/UI/ShiftDialogController.java
+++ src/com/zack/UI/ShiftDialogController.java
@@ -26,7 +26,10 @@ public class ShiftDialogController {
     @FXML
     private TextField numShiftsField;
 
-
+    /**
+     * Takes the contents of the fields and returns a new shift with those fields.
+     * @return shift that contains the fields provided in the text fields.
+     */
     public Shift getShift(){
         String date = dateField.getText();
         String dayOfWeek = dayOfWeekField.getText();
@@ -36,4 +39,31 @@ public class ShiftDialogController {
         int numShifts = Integer.parseInt(numShiftsField.getText());
         return new Shift(date, dayOfWeek, courseType, time, staffType, numShifts);
     }
+
+    /**
+     * Sets the dialog fields to the selected shift.
+     * @param selectedShift
+     */
+    public void showEditShiftDialog(Shift selectedShift){
+        dateField.setText(selectedShift.getDate());
+        dayOfWeekField.setText(selectedShift.getDayOfWeek());
+        courseTypeField.setText(selectedShift.getCourseType());
+        timeField.setText(selectedShift.getTime());
+        staffTypeField.setText(selectedShift.getStaffType());
+        numShiftsField.setText("" + selectedShift.getNumShifts());
+    }
+
+    /**
+     * Updates the selected contact with the given fields.
+     * @param selectedShift
+     */
+    public void editShift(Shift selectedShift){
+        selectedShift.setDate(dateField.getText());
+        selectedShift.setDayOfWeek(dayOfWeekField.getText());
+        selectedShift.setCourseType(courseTypeField.getText());
+        selectedShift.setTime(timeField.getText());
+        selectedShift.setStaffType(staffTypeField.getText());
+        selectedShift.setNumShifts(Integer.parseInt(numShiftsField.getText()));
+    }
+
 }
