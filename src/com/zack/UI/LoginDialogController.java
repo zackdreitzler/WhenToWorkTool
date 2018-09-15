5280a7a82f06ccf9d162d34d19c091cd0cf57b8f
ifsohn <zdreitzler@gmail.com>
2018-09-15 16:03:13 -0400

    added dialog for requesting username and password

---
+++ src/com/zack/UI/LoginDialogController.java
@@ -0,0 +1,28 @@
+package com.zack.UI;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import java.util.List;
+
+/**
+ * Dialog window to handle input of username and password.
+ *
+ * @author Zack Dreitzler
+ * @version 1.0
+ */
+public class LoginDialogController {
+    @FXML
+    private TextField username;
+
+    @FXML
+    private TextField password;
+
+
+    public void setDetails(List<String> details){
+        String user = username.getText();
+        String pass = password.getText();
+        details.add(user);
+        details.add(pass);
+    }
+}
