e1d4bee8f37643011266098eb1e2c73e36edde3d
ifsohn <zdreitzler@gmail.com>
2018-09-24 18:09:01 -0400

    completed main application

--- src/com/zack/webInterface/WebTool.java
+++ src/com/zack/webInterface/WebTool.java
@@ -42,11 +42,132 @@ public class WebTool {
      * @param shiftList the list of shifts.
      * @param schedulePage the base schedules page.
      */
-    private void addShifts(List<Shift> shiftList, HtmlPage schedulePage) {
+    private void addShifts(List<Shift> shiftList, HtmlPage schedulePage) throws FailedToolException {
         HtmlPage currentPage = schedulePage;
-        for (Shift shift : shiftList){
-            currentPage = navigateToCurrentDate(shift, currentPage);
+        for (Shift shift : shiftList) {
+            try{
+                currentPage = navigateToCurrentDate(shift, currentPage);
+                HtmlPage addShiftPage = getAddShiftPage(currentPage);
+
+                HtmlSelect jobSelector = addShiftPage.getElementByName("SkillId");
+                HtmlOption jobTitle = jobSelector.getOptionByText(getShiftTitle(shift.getStaffType()));
+                jobTitle.click();
+
+                HtmlSelect colorSelector = addShiftPage.getElementByName("Color");
+                HtmlOption colorOption = colorSelector.getOptionByText(getShiftColor(shift.getStaffType()));
+                colorOption.click();
+
+                String[] times = shift.getTime().split("-");
+                HtmlTextInput startTime = addShiftPage.getElementByName("ShiftStartTime");
+                startTime.setText(times[0]);
+                HtmlTextInput endTime = addShiftPage.getElementByName("ShiftEndTime");
+                endTime.setText(times[1]);
+
+                HtmlTextInput description = addShiftPage.getElementByName("ShiftName");
+                description.setText(shift.getCourseType().equals("") ? "Zips" : shift.getCourseType() +
+                        " Course " +
+                        (shift.getCourseType().equalsIgnoreCase("Apprentice") ? "(Apprentice)" : ""));
+
+                HtmlCheckBoxInput dayOfShift = addShiftPage.getElementByName(getDayOfShift(shift.getDayOfWeek()));
+                dayOfShift.click();
+
+                HtmlTextInput numberOfShifts = addShiftPage.getElementByName("ShiftWorkersNeeded");
+                numberOfShifts.setText("" + shift.getNumShifts());
+
+                HtmlButtonInput submit = addShiftPage.getElementByName("SubmitBtn1");
+                submit.click();
+            }catch (IOException e){
+                throw new FailedToolException("Failed to add Shift " + shift);
+            }
         }
+    }
+
+    /**
+     * Returns the color the shift will appear based on the staff type.
+     * @param staffType type of staff working the shift.
+     * @return string representing the color.
+     */
+    private String getShiftColor(String staffType) {
+        if(staffType.equalsIgnoreCase("Tower Tech")){
+            return "navy";
+        }else if(staffType.equalsIgnoreCase("Zip Tech")){
+            return "maroon";
+        }else if(staffType.equalsIgnoreCase("Facilitator")){
+            return "purple";
+        }else {
+            return "aqua";
+        }
+    }
+
+    /**
+     * Returns the title of the position found on whentowork.
+     * @param staffType type of staff for this shift.
+     * @return string on the correct position.
+     */
+    private String getShiftTitle(String staffType) {
+        if(staffType.equalsIgnoreCase("Tower Tech")){
+            return "Team Training Tower Tech";
+        }else if(staffType.equalsIgnoreCase("Zip Tech")){
+            return "Team Training Zip Tech";
+        }else if(staffType.equalsIgnoreCase("Facilitator")){
+            return "Team Training Facilitator";
+        }else {
+            return "Team Training Apprentice";
+        }
+    }
+
+    /**
+     * Returns the correct checkbox based on the day of week the shift occurs.
+     * @param dayOfWeek string to represent the day of week the shift is on.
+     * @return String name of the correct checkbox.
+     */
+    private String getDayOfShift(String dayOfWeek) {
+        switch (dayOfWeek){
+            case "Su":
+                return "AddDay0";
+            case "M":
+                return "AddDay1";
+            case "Tu":
+                return "AddDay2";
+            case "W":
+                return "AddDay3";
+            case "Th":
+                return "AddDay4";
+            case "F":
+                return "AddDay5";
+            case "Sa":
+                return "AddDay6";
+            default:
+                return null;
+        }
+    }
+
+    /**
+     * Opens the add shifts page on whentowork.
+     * @param currentPage page the application is currently on
+     * @return the page that has the Add Shifts form on it.
+     * @throws FailedToolException if IOException is thrown.
+     */
+    private HtmlPage getAddShiftPage(HtmlPage currentPage) throws FailedToolException{
+            HtmlTable table = null;
+            final DomNodeList inputs = currentPage.getElementsByTagName("table");
+            final Iterator<DomNode> iter = inputs.iterator();
+            while(iter.hasNext()){
+                HtmlTable tempTable = (HtmlTable) iter.next();
+                if(!tempTable.getBgcolorAttribute().equals("")) table = tempTable;
+            }
+
+            HtmlTableRow row = table.getRow(0);
+            HtmlTableCell addShiftsCell = null;
+            for(HtmlTableCell tempCell : row.getCells()){
+                if(tempCell.toString().contains("Add Shifts")) addShiftsCell = tempCell;
+            }
+
+            try{
+                return addShiftsCell.click();
+            }catch (IOException e){
+                throw new FailedToolException("Failed to open Add Shifts page.");
+            }
     }
 
     /**
@@ -57,17 +178,72 @@ public class WebTool {
      */
     private HtmlPage navigateToCurrentDate(Shift shift, HtmlPage currentPage) throws FailedToolException{
         String[] date = shift.getDate().split("/");
+        String month = getMonth(date[0]);
         String day = date[1];
         int count = 0;
         while(count < 52){
             HtmlTable dateTable = currentPage.getHtmlElementById("daysTable");
             HtmlTableRow dateRow = dateTable.getRow(0);
             for (HtmlTableCell cell : dateRow.getCells()){
-                if(cell.asText().contains(day)) return currentPage;
+                if(cell.asText().contains(month+"-"+day)){
+                    return currentPage;
+                }
             }
+
+            HtmlAnchor anchor = null;
+            Iterator<HtmlAnchor> iterator = currentPage.getAnchors().iterator();
+            while (iterator.hasNext()){
+                anchor = iterator.next();
+                if(anchor.toString().contains("Forward one week")){
+                    try {
+                        currentPage = anchor.click();
+                        break;
+                    } catch (IOException e){
+                        throw new FailedToolException("Failed to advance to the next week\n" +
+                                "Current URL is " + currentPage.getUrl());
+                    }
+                }
+            }
+
             count++;
         }
         throw new FailedToolException("Date not Found: " + shift.getDate());
+    }
+
+    /**
+     * Returns the appropriate string for the month of this shift.
+     * @param str the month of this shift
+     * @return
+     */
+    private String getMonth(String str) {
+        switch (str){
+            case "1":
+                return "Jan";
+            case "2":
+                return "Feb";
+            case "3":
+                return "Mar";
+            case "4":
+                return "Apr";
+            case "5":
+                return "May";
+            case "6":
+                return "Jun";
+            case "7":
+                return "Jul";
+            case "8":
+                return "Aug";
+            case "9":
+                return "Sep";
+            case "10":
+                return "Oct";
+            case "11":
+                return "Nov";
+            case "12":
+                return "Dec";
+            default:
+                return null;
+        }
     }
 
     /**
